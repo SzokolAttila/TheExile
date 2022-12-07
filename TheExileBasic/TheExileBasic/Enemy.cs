903b88bbd15892666e17ef95e588a509664f23ec
SzokolAttila <74241929+SzokolAttila@users.noreply.github.com>
2022-12-07 12:14:54 +0100

    Classes are now in seperate files

---
+++ TheExileBasic/TheExileBasic/Enemy.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TheExileBasic
+{
+    class Enemy
+    {
+        public static List<Enemy> Enemies = new List<Enemy>();
+        public int HP { get; set; }
+        public int AP { get; set; }
+        public string Name { get; set; }
+        public int XP { get; set; }
+        public int[] Pos { get; set; }
+        public Enemy(int hp, int ap, string name, int xp, int[] pos, string[,] room)
+        {
+            Enemies.Add(this);
+            this.HP = hp;
+            this.AP = ap;
+            this.XP = xp;
+            this.Name = name;
+            this.Pos = pos;
+            room[this.Pos[0], this.Pos[1]] = "!";
+        }
+        public static void CheckPositions(Fighter fighter)
+        {
+            for (int i = 0; i < Enemies.Count; i++)
+            {
+                if ((fighter.Pos[0] == Enemies[i].Pos[0] && fighter.Pos[1] - 1 == Enemies[i].Pos[1]) || (fighter.Pos[0] == Enemies[i].Pos[0] && fighter.Pos[1] + 1 == Enemies[i].Pos[1]) || (fighter.Pos[0] - 1 == Enemies[i].Pos[0] && fighter.Pos[1] == Enemies[i].Pos[1]) || (fighter.Pos[0] + 1 == Enemies[i].Pos[0] && fighter.Pos[1] == Enemies[i].Pos[1]))
+                    Console.WriteLine("\nYou found yourself in front of a(n) " + Enemies[i].Name + " with " + Enemies[i].HP + " Health Points and " + Enemies[i].AP + " Attack Points.\nStep on the same field for combat.");
+                else if (fighter.Pos[0] == Enemies[i].Pos[0] && fighter.Pos[1] == Enemies[i].Pos[1])
+                {
+                    int[] result = Enemies[i].Combat(fighter);
+                    fighter.HP = result[0];
+                    fighter.XP += result[1];
+                    fighter.Temp = "0";
+                    Enemies.Remove(Enemies[i]);
+                    Console.Clear();
+                }
+            }
+        }
+        public int[] Combat(Fighter fighter)
+        {
+            fighter.Fought = true;
+            int currentHP = this.HP;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("The Exile\n");
+                Console.WriteLine("Now it's time to fight!");
+                Console.WriteLine(this.Name + "\t\t\t\t" + "Your stats:\n" + currentHP + "/" + this.HP + "\t\t\t\t" + "Attack: " + fighter.Attack + "\n\t\t\t\t" + "HP: " + fighter.HP);
+                Console.WriteLine("It's your turn");
+                currentHP -= fighter.Attack;
+                if (currentHP <= 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("The Exile\n");
+                    Console.WriteLine("Now it's time to fight!");
+                    Console.WriteLine(this.Name + "\t\t\t\t" + "Your stats:\n" + currentHP + "/" + this.HP + "\t\t\t\t" + "Attack: " + fighter.Attack + "\n\t\t\t\t" + "HP: " + fighter.HP);
+                    Console.WriteLine("You have won!\nPress any key to end this scene:");
+                    Console.ReadKey();
+                    return new int[] { fighter.HP, this.XP };
+                }
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("The Exile\n");
+                Console.WriteLine("Now it's time to fight!");
+                Console.WriteLine(this.Name + "\t\t\t\t" + "Your stats:\n" + currentHP + "/" + this.HP + "\t\t\t\t" + "Attack: " + fighter.Attack + "\n\t\t\t\t" + "HP: " + fighter.HP);
+                Console.WriteLine("It's the enemy's turn");
+                fighter.HP -= this.AP;
+                if (fighter.HP <= 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("The Exile\n");
+                    Console.WriteLine("Now it's time to fight!");
+                    Console.WriteLine(this.Name + "\t\t\t\t" + "Your stats:\n" + currentHP + "/" + this.HP + "\t\t\t\t" + "Attack: " + fighter.Attack + "\n\t\t\t\t" + "HP: " + fighter.HP);
+                    Console.WriteLine("You have lost!\nPress any key to end this scene:");
+                    Console.ReadKey();
+                    return new int[] { fighter.HP, 0 };
+                }
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Press any key to end this scene:");
+            Console.ReadKey(true);
+            return new int[] { fighter.Attack, this.XP };
+        }
+    }
+}
---
+++ TheExileBasic/TheExileBasic/Fighter.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TheExileBasic
+{
+    class Fighter
+    {
+        public List<Item> Inventory { get; set; }
+        public List<string> Names { get; set; }
+        public string Temp { get; set; }
+        public int Range { get; set; }
+        public int[] Pos { get; set; }
+        public int Attack { get; set; }
+        public int HP { get; set; }
+        public int MaxHP { get; set; }
+        public int XP { get; set; }
+        public bool Fought { get; set; }
+
+        public Fighter(int range, int[] pos, int attack, int hp, int xp = 0)
+        {
+            this.Fought = false;
+            this.Inventory = new List<Item>();
+            this.Names = new List<string>();
+            this.Range = range;
+            this.Pos = pos;
+            this.Attack = attack;
+            this.HP = hp;
+            this.XP = xp;
+            this.MaxHP = hp;
+        }
+
+        public string[,] Move(string direction, string[,] room)
+        {
+            room[this.Pos[0], this.Pos[1]] = this.Temp;
+
+            switch (direction)
+            {
+                case "s":
+                    if (this.Pos[0] < room.GetLength(0) - 1 && room[this.Pos[0] + 1, this.Pos[1]] != "M" && (room[this.Pos[0] + 1, this.Pos[1]] != "~" || this.Names.Contains("Boat")) && room[this.Pos[0] + 1, this.Pos[1]] != " ")
+                        this.Pos[0]++;
+                    break;
+                case "w":
+                    if (this.Pos[0] > 0 && room[this.Pos[0] - 1, this.Pos[1]] != "M" && (room[this.Pos[0] - 1, this.Pos[1]] != "~" || this.Names.Contains("Boat")) && room[this.Pos[0] - 1, this.Pos[1]] != " ")
+                        this.Pos[0]--;
+                    break;
+                case "a":
+                    if (this.Pos[1] > 0 && room[this.Pos[0], this.Pos[1] - 1] != "M" && (room[this.Pos[0], this.Pos[1] - 1] != "~" || this.Names.Contains("Boat")) && room[this.Pos[0], this.Pos[1] - 1] != " ")
+                        this.Pos[1]--;
+                    break;
+                case "d":
+                    if (this.Pos[1] < room.GetLength(1) - 1 && room[this.Pos[0], this.Pos[1] + 1] != "M" && (room[this.Pos[0], this.Pos[1] + 1] != "~" || this.Names.Contains("Boat")) && room[this.Pos[0], this.Pos[1] + 1] != " ")
+                        this.Pos[1]++;
+                    break;
+                default:
+                    break;
+            }
+
+            Thread.Sleep(1);
+            if (room[this.Pos[0], this.Pos[1]] == "~")
+            {
+                this.Temp = room[this.Pos[0], this.Pos[1]];
+                room[this.Pos[0], this.Pos[1]] = "B";
+            }
+            else
+            {
+                this.Temp = room[this.Pos[0], this.Pos[1]];
+                room[this.Pos[0], this.Pos[1]] = "X";
+            }
+            Console.Clear();
+
+            return room;
+        }
+
+        public void View(string[,] room)
+        {
+            for (int i = -this.Range; i <= this.Range; i++)
+            {
+                for (int j = -this.Range; j <= this.Range; j++)
+                {
+                    if (this.Pos[0] + i >= 0 && this.Pos[0] + i < room.GetLength(0) && this.Pos[1] + j >= 0 && this.Pos[1] + j < room.GetLength(1) && this.Pos[1] + j < room.GetLength(1))
+                    {
+                        switch (room[this.Pos[0] + i, this.Pos[1] + j])
+                        {
+                            case "X":
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                break;
+                            case "0":
+                                Console.ForegroundColor = ConsoleColor.DarkGray;
+                                break;
+                            case "M":
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                                break;
+                            case "!":
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                break;
+                            case "*":
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                break;
+                            case "~":
+                                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                                break;
+                            case "B":
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                break;
+                            default:
+                                Console.ForegroundColor = ConsoleColor.White;
+                                break;
+                        }
+                        Console.Write(room[this.Pos[0] + i, this.Pos[1] + j]);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Map(string[,] room)
+        {
+            for (int i = 0; i < room.GetLength(0); i++)
+            {
+                for (int j = 0; j < room.GetLength(1); j++)
+                {
+                    switch (room[i, j])
+                    {
+                        case "X":
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            break;
+                        case "0":
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            break;
+                        case "M":
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            break;
+                        case "!":
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            break;
+                        case "*":
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            break;
+                        case "~":
+                            Console.ForegroundColor = ConsoleColor.DarkBlue;
+                            break;
+                        case "B":
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                    }
+                    Console.Write(room[i, j]);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
